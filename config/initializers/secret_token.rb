d2a99cd5df441aae71c0c3e2161aa16c1693bdf0
Joe Weakley <joew@timjoe.com>
2010-12-20 17:53:02 -0800

    Fixed the application to the correct version rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bv::Application.config.secret_token = '66864015fdccdaa2653c33a54c69416d7ccc9dd44d6aecebef7b653c7783bca27f5766681219574ed22bb9bdcd9db7aa96f1a1f1f79e34ea64d6370df107f2a1'
