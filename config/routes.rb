dc164180e6b65e3401c4560543d0f2f43854d9a5
Joe Weakley <joe.weakley@digitalaugment.com>
2010-12-22 17:02:54 -0800

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Bv::Application.routes.draw do
+  resources :users
+  resource :session
+  match '/login' => "sessions#new", :as => "login"
+  match '/logout' => "sessions#destroy", :as => "logout"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
