d5ab59e9429de0345e9970b6672f41a295f5cd65
Mike O'Toole <mike.otoole@digitalaugment.com>
2011-01-15 13:14:48 -0800

    Made migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110115184600) do
+ActiveRecord::Schema.define(:version => 20110115211026) do
 
   create_table "acknowledgment_of_announcements", :force => true do |t|
     t.integer  "announcement_id"
@@ -156,13 +156,18 @@ ActiveRecord::Schema.define(:version => 20110115184600) do
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
+  create_table "site_announcements", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "system_resources", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "team_speaks", :force => true do |t|
+  create_table "teamspeaks", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
