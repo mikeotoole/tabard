7e72805bb76ea17eddcb136b6d4cd31deb7f13fd
Bryan Rogers <bryanrogers@RHEL6Workstation.(none)>
2011-02-11 19:44:37 -0800

    Updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,20 @@
 set :application, "Brutal Venom"
-set :repository,  "set your repository location here"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository,  "git.digitalaugment.com:da-bv-rails.git"
+set :scm, :git
+set :scm, "git"
+set :user, "$USER"
+set :scm_passphrase, "$PASSPHRASE"
+set :ssh_options, {:forward_agent => true}
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/var/www/brutalvenom"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+server "digitalaugment.com", :app, :web, :db, :primary => true
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
