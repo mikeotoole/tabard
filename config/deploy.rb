6b8a169b6754758417760fc84567cec93c9389d9
Bryan Rogers <bryanrogers@RHEL6Workstation.(none)>
2011-02-20 13:30:26 -0800

    Updates

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,45 @@
-set :application, "Brutal Venom"
+# Main Details
+set :application, "brutalvenom.digitalaugment.com"
+role :web, "brutalvenom.digitalaugment.com"
+role :app, "brutalvenom.digitalaugment.com"
+role :db,  "brutalvenom.digitalaugment.com", :primary => true
 
-set :scm, :git
-set :repository,  "digitalaugment-deploy:da-bv-rails"
-set :branch, "master"
-set :user, "deploy"
-set :scm_passphrase, "Deploy4DApP%#!"
-ssh_options[:keys] = %w(~/.ssh/deploy@DevelopmentServer)
+# Server Details
+set :default_run_options, {:pty => true}
+set :ssh_options, {:keys => %w(~/.ssh/deploy@DevelopmentServer)}
 set :ssh_options, {:forward_agent => true}
+set :deploy_to, "/var/www/deploy/brutalvenom"
+set :deploy_via, :remote_cache
+set :user, "deploy"
+set :use_sudo, false
 
-set :deploy_to, "/var/www/brutalvenom"
+# Repo Details
+set :scm, :git
+set :scm_username, "deploy"
+set :scm_passphrase, "Deploy4DApP%#!"
+set :repository, "digitalaugment-deploy:da-bv-rails"
+set :branch, "master"
+set :git_enable_submodules, 1
 
-server "digitalaugment.com", :app, :web, :db, :primary => true
-
+# Tasks
 namespace :deploy do
   task :start, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
+    run "touch #{current_path}/tmp/restart.txt"
   end
 
   task :stop, :roles => :app do
-    # Do Nothing
+    # Do nothing.
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Symlink Shared Resources on Each Release"
+  task :symlink_shared, :roles => :app do
+    #run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 end
+
+after 'deploy:update_code', 'deploy:symlink_shared'
