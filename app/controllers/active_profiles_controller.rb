f227d2be0ea22f1a831ba9c3eedb81d44acb5673
Mike O'Toole <mike.otoole@digitalaugment.com>
2011-03-11 17:26:49 -0800

    Fixed bug in the active profile create controller.

--- app/controllers/active_profiles_controller.rb
+++ app/controllers/active_profiles_controller.rb
@@ -11,7 +11,7 @@ class ActiveProfilesController < ApplicationController
       session[:character_id] = params[:active_profile][:character_id]
       session[:profile_id] = CharacterProxy.find_by_character_id(params[:active_profile][:character_id]).active_profile_id
     end
-    active_profile_name = current_character ? current_character.name : user.name
+    active_profile_name = current_character ? current_character.name : current_user.name
     redirect_to root_path, :notice => "Profile <em>#{active_profile_name}</em> activated."
   end
   
