b62a17a9c69ee77babf3feb4396f139c4e405830
Joe Weakley <joe.weakley@digitalaugment.com>
2011-06-01 16:57:20 -0700

    Updates to forms for BVR-66

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -7,6 +7,33 @@ class Submission < ActiveRecord::Base
   
   accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true  
   
+  def contains_predefined_answer(panswer)
+    self.answers.each do |answer|
+      if answer.question_id == panswer.question_id and answer.content == panswer.content
+        answer.id = -answer.question_id unless answer.id
+        return answer
+      end
+    end
+    nil
+  end
+  
+  def contains_predefined_answer_from_collection(panswer_collection)
+    panswer_collection.each do |panswer|
+      return self.contains_predefined_answer(panswer) if self.contains_predefined_answer(panswer) != nil
+    end
+    nil
+  end
+  
+  def contains_answer_to_question(question)
+    self.answers.each do |answer|
+      if answer.question_id == question.id
+        answer.id = -answer.question_id unless answer.id
+        return answer
+      end
+    end
+    nil
+  end
+  
   def name
     self.user_profile.name
   end
