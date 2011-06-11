72ab8881a6acce46bfbc9330a88147c02adcc3b1
Joe Weakley <joe.weakley@digitalaugment.com>
2011-06-11 14:42:01 -0700

    Updated for Doug

--- app/controllers/discussion_spaces_controller.rb
+++ app/controllers/discussion_spaces_controller.rb
@@ -5,7 +5,11 @@ class DiscussionSpacesController < ApplicationController
   # GET /discussion_spaces.xml
   def index
       @discussion_spaces = DiscussionSpace.order("system DESC, name ASC")
-      respond_with(@discussion_spaces)
+      if !current_user.can_show("DiscussionSpace")
+        render_insufficient_privileges
+      else
+        respond_with(@discussion_spaces)
+      end
   end
 
   # GET /discussion_spaces/1
@@ -14,23 +18,97 @@ class DiscussionSpacesController < ApplicationController
     @discussion_space = DiscussionSpace.find(params[:id])
     
     if !current_user.can_show(@discussion_space)
-      render :nothing => true, :status => :forbidden
+      render_insufficient_privileges
     else   
       # Site Announcement Space
       if(@discussion_space.system and @discussion_space.announcement_space and @discussion_space.game == nil)
-        render :file => 'discussion_spaces/site_announcement_space'
+        render :partial => 'discussion_spaces/site_announcement_space'
         return
       # Game Announcement Space
       elsif(@discussion_space.system and @discussion_space.announcement_space and @discussion_space.game != nil)
-        render :file => 'discussion_spaces/game_announcement_space'
+        render :partial => 'discussion_spaces/game_announcement_space'
         return
       # Registraion Appication Space
       elsif(@discussion_space.system and @discussion_space.registration_application_space != nil)
         @registration_applications = RegistrationApplication.all_new
         @form = SiteForm.application_form
-        render :file => 'discussion_spaces/registration_application_space'
+        render :partial => 'discussion_spaces/registration_application_space'
         return  
       end  
+      respond_with(@discussion_space)
+    end
+  end
+  
+  # GET /discussion_spaces/new
+  # GET /discussion_spaces/new.xml
+  def new
+    @discussion_space = DiscussionSpace.new
+    if !current_user.can_create(@discussion_space)
+      render_insufficient_privileges
+    else
+      respond_with(@discussion_space)
+    end
+  end
+
+  # GET /discussion_spaces/1/edit
+  def edit
+    @discussion_space = DiscussionSpace.find(params[:id])
+    if !current_user.can_update(@discussion_space)
+      render_insufficient_privileges
+    end
+    respond_with(@discussion_space)
+  end
+
+  # POST /discussion_spaces
+  # POST /discussion_spaces.xml
+  def create
+    @discussion_space = DiscussionSpace.new(params[:discussion_space])
+    @discussion_space.system = false
+    @discussion_space.user_profile = current_user.user_profile
+    if !current_user.can_create(@discussion_space)
+      render_insufficient_privileges
+    else
+      if @discussion_space.save
+        add_new_flash_message('Discussion space was successfully created.')
+        respond_with(@discussion_space)
+      else
+        respond_to do |format|
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @discussion_space.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  # PUT /discussion_spaces/1
+  # PUT /discussion_spaces/1.xml
+  def update
+    @discussion_space = DiscussionSpace.find(params[:id])
+    if !current_user.can_update(@discussion_space)
+      render_insufficient_privileges
+    else
+      if @discussion_space.update_attributes(params[:discussion_space])
+        add_new_flash_message('Discussion space was successfully updated.')
+        respond_with(@discussion_space)
+      else
+        respond_to do |format|
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @discussion_space.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+  end
+
+  # DELETE /discussion_spaces/1
+  # DELETE /discussion_spaces/1.xml
+  def destroy
+    @discussion_space = DiscussionSpace.find(params[:id])
+    if !current_user.can_delete(@discussion_space)
+      render_insufficient_privileges
+    else 
+      if @discussion_space.destroy
+        add_new_flash_message('Discussion space was successfully deleted.')
+      end
       respond_with(@discussion_space)
     end
   end
