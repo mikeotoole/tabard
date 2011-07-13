ae4827211e0f4c970577d5673c6899ef928393a1
Bryan Rogers <bryan.rogers@digitalaugment.com>
2011-07-13 16:29:55 -0700

    NO HAX

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       :provider           => 'Rackspace',
       :rackspace_username => 'digitalaugment',
-      :rackspace_api_key  => 'b4cb915dbfce8d25deb9d35c5ade15c8'
+      :rackspace_api_key  => '40c425c1543efb4a0f565258f9c6ea35'
     }
     config.fog_host = "http://c655941.r41.cf2.rackcdn.com"
     config.fog_directory = 'Crumblin-Production'
@@ -13,7 +13,7 @@ else
     config.fog_credentials = {
       :provider           => 'Rackspace',
       :rackspace_username => 'digitalaugment',
-      :rackspace_api_key  => 'b4cb915dbfce8d25deb9d35c5ade15c8'
+      :rackspace_api_key  => '40c425c1543efb4a0f565258f9c6ea35'
     }
     config.fog_host = "http://c655790.r90.cf2.rackcdn.com"
     config.fog_directory = 'Crumblin-Development'
