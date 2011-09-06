c4179544936ccb9e9208d458b912117a8ac0193d
Joe Weakley <joe.weakley@digitalaugment.com>
2011-09-06 14:44:26 -0700

    Quick commit before I switch branches...

--- spec/requests/roles_spec.rb
+++ spec/requests/roles_spec.rb
@@ -4,8 +4,7 @@ describe "Roles" do
   describe "GET /roles" do
     it "should display all roles for the community" do
       user = Factory(:user)
-      community = Factory(:community)
-
+      community = Factory(:community, :admin_profile => user.user_profile)
       visit community_path(community)
     end
   end
