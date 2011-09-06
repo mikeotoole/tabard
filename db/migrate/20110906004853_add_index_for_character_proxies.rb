bf15f1e346820411f8fc5028ba4801597354e07d
Mike O'Toole <mike.otoole@digitalaugment.com>
2011-09-06 10:52:08 -0700

    Added migration for indexes.

--- db/migrate/20110906004853_add_index_for_character_proxies.rb
+++ db/migrate/20110906004853_add_index_for_character_proxies.rb
@@ -1,11 +1,6 @@
 class AddIndexForCharacterProxies < ActiveRecord::Migration
-  def up
+  def change
     add_index :character_proxies, :user_profile_id
     add_index :character_proxies, [:character_type, :character_id]
-  end
-
-  def down
-    remove_index :user_profile_id
-    remove_index [:character_type, :character_id]
   end
 end
