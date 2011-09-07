0c2f0b8d179b51818ee183835ba60c12cbca6012
Mike O'Toole <mike.otoole@digitalaugment.com>
2011-09-07 02:41:43 -0700

    Have all tests finshed.

--- spec/controllers/communities_controller_spec.rb
+++ spec/controllers/communities_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe CommunitiesController do
   let(:user) { Factory.create(:user) }
-  let(:community_att) { Factory.attributes_for(:community)}
+  let(:community_att) { Factory.attributes_for(:community, :name => "TestName")}
   let(:community) { Factory.create(:community)}
 
   describe "GET 'index'" do
@@ -19,14 +19,14 @@ describe CommunitiesController do
   end
   
   describe "GET 'show'" do
-    it "should redirect to community subdomain home when not authenticated as a user" do
-      get 'show', :id => community.id
+    it "show should redirect to subdomain home when authenticated as a user" do
+      sign_in user
+      get 'show', :id => community
       response.should redirect_to(subdomain_home_url(:subdomain => community.subdomain))
     end
   
-    it "show should redirect to subdomain home when authenticated as a user" do
-      sign_in user
-      get 'show', :id => community.id
+    it "should redirect to community subdomain home when not authenticated as a user" do
+      get 'show', :id => community
       response.should redirect_to(subdomain_home_url(:subdomain => community.subdomain))
     end
   end
@@ -40,88 +40,85 @@ describe CommunitiesController do
   
     it "shouldn't be successful when not authenticated as a user" do
       get 'new'
-      response.should redirect_to(new_user_session_path) 
+      response.should redirect_to(new_user_session_path)
+    end
+  end
+  
+  describe "GET 'edit'" do
+    it "should be successful when authenticated as a user" do
+      sign_in user
+      get 'edit', :id => community
+      assert_response :success
+    end
+    
+    it "should redirected to new user session path when not authenticated as a user" do
+      get 'edit', :id => community
+      response.should redirect_to(new_user_session_path)
     end
   end
   
   describe "POST 'create' authenticated as a user" do
     before(:each) do
       sign_in user
-      @community = community
-      post 'create'
+      post 'create', :community => community_att
     end
     
-    it "should be successful" do
-      response.should be_success
+    it "should not be new record" do
+      assigns[:community].should_not be_new_record
     end
     
-    it "should be new record" do
-      assigns[:community].should be_new_record
+    it "should pass params to community" do
+      assigns[:community].name.should == 'TestName'
     end
     
-    it "should redirect to community path" do
-      #TODO Mike, What is the syntax for this?
-#       response.should redirect_to(community_path(@community))
+    it "should redirect to new community" do
+      response.should redirect_to(community_path(assigns[:community]))
     end
   end
   
   describe "POST 'create' when not authenticated as a user" do
     before(:each) do
-      @community = community
-      post 'create'
-    end
-    
-    it "shouldn't be successful" do
-      response.should_not be_success
+      post 'create', :community => community_att
     end
     
     it "should not create new record" do
-      assigns[:community].should == nil
+      assigns[:community].should be_nil
     end
     
     it "should redirect to new user session path" do
       response.should redirect_to(new_user_session_path)
     end
   end   
-  
-  describe "GET 'edit' when authenticated as a user" do
+
+  describe "PUT 'update' when authenticated as a user" do
     before(:each) do
+      @new_slogan = 'My new slogan.'
       sign_in user
-      get 'edit', :id => community.id
+      put 'update', :id => community, :community => { :slogan => @new_slogan }
     end
   
-    it "should be successful" do
-      assert_response :success
-    end
-  end
-  
-  describe "GET 'edit' when not authenticated as a user" do
-    before(:each) do
-      get 'edit', :id => community.id
-    end
-  
-    it "shouldn't be successful" do
-      response.should_not be_success
+    it "should change attributes" do
+      assigns[:community].slogan.should == @new_slogan
     end
     
-    it "should redirected to new user session path" do
-      response.should redirect_to(new_user_session_path)
+    it "should redirect to new community" do
+      response.should redirect_to(community_path(assigns[:community]))
     end
-  end
-
-  describe "PUT 'update'" do
-    it "should be successful when authenticated as a user" do
-      sign_in user
-      put 'update', :id => community.id, :community => community_att
-      response.should be_success
-#       assert_redirected_to community_path(assigns(:community))
-    end
+  end  
   
-    it "should redirect to new user session path when not authenticated as a user" do
-      put 'update', :id => community.id, :community => community_att
-      response.should redirect_to(new_user_session_path)
+  describe "PUT 'update' when not authenticated as a user" do
+    before(:each) do
+      put 'update', :id => community, :community => { :slogan => "New Slogan" }
     end
-  end    
+    
+    it "should redirect to new user session path" do
+      response.should redirect_to(new_user_session_path)
+    end  
+    
+    it "should not change attributes" do
+      assigns[:community].should be_nil
+    end    
+  end 
 
   describe "DELETE 'destroy'" do
     #TODO Add 404 redirect for all routing errors
@@ -129,6 +126,7 @@ describe CommunitiesController do
 #       sign_in user
 #       @community = community
 #       delete 'destroy'
+#       response.code.should == "404"
 #       response.should redirect_to("/404")
 #     end
 # 
--- spec/controllers/subdomains_controller_spec.rb
+++ spec/controllers/subdomains_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe SubdomainsController do
-  # TODO Joe add test for subdomain controller.
-#   describe "GET 'index'" do
-#     it "should be successful" do
-#       get 'index'
-#       response.should be_success
-#     end
-#   end
+  let(:user) { Factory.create(:user) }
+
+  # TODO Joe, Are this tests right? When should it redirect to subdomain?
+  describe "GET 'index'" do
+    it "should redirect to root url when authenticated as a user" do
+      sign_in user
+      get 'index'
+      response.should redirect_to(root_url)
+    end
+    
+    it "should redirected to new user session path when not authenticated as a user" do
+      get 'index'
+      response.should redirect_to(new_user_session_path)
+    end
+  end
 end
