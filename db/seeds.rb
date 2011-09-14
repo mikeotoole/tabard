c72aaa6c44212ddcb5c7a984f3ae0b195e3247c5
Joe Weakley <joe.weakley@digitalaugment.com>
2011-09-14 15:01:40 -0700

    Updated seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,12 @@ s_turtle = User.new(:email => "snappy@turtle.com", :password => "Password",
 s_turtle.skip_confirmation!
 s_turtle.save
 
+puts "Creating Dirty Badger!"
+d_badger = User.new(:email => "dirty@badger.com", :password => "Password",
+                    :user_profile_attributes => {:first_name => "Dirty", :last_name => "Badger"})
+d_badger.skip_confirmation!
+d_badger.save
+
 puts "RoboBilly is creating Just Another Headshot Clan..."
 jahc = robobilly.owned_communities.create(:name => "Just Another Headshot", :slogan => "Boom baby!", :label => "Clan")
 
@@ -35,9 +41,10 @@ noob_role.permissions.create(:subject_class => "Role", :permission_level => "Sho
 
 # TODO Mike/Joe Make DMoose + STurtle Apply to the community -JW
 
-puts "Making Diabolical Moose and Snappy Turtle members of Just Another Headshot Clan..."
+puts "Making Diabolical Moose, Snappy Turtle and Dirty Badger members of Just Another Headshot Clan..."
 jahc.promote_user_profile_to_member(d_moose.user_profile)
 jahc.promote_user_profile_to_member(s_turtle.user_profile)
+jahc.promote_user_profile_to_member(d_badger.user_profile)
 
 puts "Giving D-Moose the n00b role..."
 d_moose.add_new_role(noob_role)
