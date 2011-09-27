9184a675b265e2863d9b6a1d242dce3c16a7ec95
Mike O'Toole <mike.otoole@digitalaugment.com>
2011-09-27 14:04:56 -0700

    added files for pages.

---
+++ db/migrate/20110927203012_create_pages.rb
@@ -0,0 +1,18 @@
+class CreatePages < ActiveRecord::Migration
+  def change
+    create_table :pages do |t|
+      t.string :name
+      t.text :body
+      t.integer :character_proxy_id
+      t.integer :user_profile_id
+      t.integer :page_space_id
+      t.boolean :show_in_navigation, :default => false
+
+      t.timestamps
+    end
+    
+    add_index :pages, :character_proxy_id
+    add_index :pages, :user_profile_id
+    add_index :pages, :page_space_id
+  end
+end
