c858d03485b55205fa0e6c9af11179e86d8a5206
Douglas Waltman II <doug@dougwaltman.com>
2011-11-07 15:09:11 -0800

    changed 'roster assigments' to 'roster membership'

--- app/controllers/subdomains_controller.rb
+++ app/controllers/subdomains_controller.rb
@@ -44,7 +44,7 @@ class SubdomainsController < ApplicationController
     #application
     management_items << {:link => edit_community_settings_path, :title => "Community Settings"} if can_manage(current_community)
     management_items << {:link => community_applications_path, :title => "Applications - #{current_community.pending_applications.size}"} if can_manage(current_community.community_applications.new())
-    management_items << {:link => pending_roster_assignments_url, :title => "Roster Assigments"} if can_manage(current_community.pending_roster_assignments.new)
+    management_items << {:link => pending_roster_assignments_url, :title => "Roster Membership"} if can_manage(current_community.pending_roster_assignments.new)
     management_items << {:link => custom_forms_url, :title => "Forms"} if can_manage(current_community.custom_forms.new)
     management_items << {:link => roles_url, :title => "Permissions"} if can_manage(current_community.roles.new)
     management_items
