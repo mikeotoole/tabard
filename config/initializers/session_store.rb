3e5e78d01cba58e348b98de96f2d90aabe1f4a1c
Douglas Waltman II <doug@dougwaltman.com>
2011-11-09 13:04:42 -0800

    minor tweaks

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,7 +2,7 @@
 if Rails.env.production?
   DaBvRails::Application.config.session_store :mem_cache_store # TODO Ensure this works on all domains
 else
-  DaBvRails::Application.config.session_store :cookie_store, :key => '_da-bv-rails_session', :domain => '.lvh.me'
+  DaBvRails::Application.config.session_store :cookie_store, :key => '_da-bv-rails_session', :domain => "lvh.me"
 end
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
