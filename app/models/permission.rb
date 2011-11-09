602cbe78a5a4e3af0aafe0a84f6a7060c08932ef
Douglas Waltman II <doug@dougwaltman.com>
2011-11-08 16:08:55 -0800

    updated seed and perm forms.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -10,9 +10,9 @@ class Permission < ActiveRecord::Base
 # Constants
 ###
   # This is a collection of strings that are valid for subject classes.
-  VALID_SUBJECT_CLASSES = %w( Role CustomForm )
+  VALID_SUBJECT_CLASSES = %w( Role CustomForm CommunityApplication )
   # This is a collection of strings that are valid for permission levels.
-  VALID_PERMISSION_LEVELS = %w( View Update Create Delete )
+  VALID_PERMISSION_LEVELS = %w( View Create Update Delete )
 
 ###
 # Associations
