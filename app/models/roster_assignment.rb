a3865abff741e53abe7731cef1cf72a2eb11df4c
Douglas Waltman II <doug@dougwaltman.com>
2011-11-10 23:44:22 -0800

    stupid bug, for the love of derp

--- app/models/roster_assignment.rb
+++ app/models/roster_assignment.rb
@@ -24,10 +24,10 @@ class RosterAssignment < ActiveRecord::Base
 # Delegates
 ###
   delegate :user_profile, :to => :community_profile, :allow_nil => true
-  delegate :avatar_url, :to => :user_profile, :prefix => true
   delegate :community_admin_profile_id, :to => :community_profile, :allow_nil => true
   delegate :name, :avatar_url, :to => :character_proxy, :prefix => true
   delegate :display_name, :to => :user_profile, :prefix => true
+  delegate :avatar_url, :to => :user_profile, :prefix => true
 
 ###
 # Callbacks
