f2e7896b87e5754f6447684948815f4b2d71bb03
Douglas Waltman II <doug@dougwaltman.com>
2011-11-29 23:23:05 -0800

    seed is still der-broken, MIKE ....

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-
+require 'swtor_servers_list'
 require 'wow_servers_list'
 
 if ENV["RAILS_ENV"] != 'test'
 
   Timecop.freeze(2.months.ago)
 
-  # Create a default user
   puts "Creating test active admin users"
   superadmin = AdminUser.create(:email => 'superadmin@example.com', :password => 'Password', :password_confirmation => 'Password', :role => "superadmin")
   moderator = AdminUser.create(:email => 'moderator@example.com', :password => 'Password', :password_confirmation => 'Password', :role => "moderator")
--- lib/swtor_servers_list.rb
+++ lib/swtor_servers_list.rb
@@ -6,13 +6,14 @@
 # This is a list of all valid swtor servers used for the inital seed.
 ###
 module SwtorServersList
-  VALID_SERVERS =
-    [["Belgoth's Beacon", 'PvE'],
+  VALID_SERVERS = [
+    ["Belgoth's Beacon", 'PvE'],
     ["Bondar Crystal", 'PvE'],
     ["Jedi Covenant", 'PvE'],
     ["Sylvar", 'PvE'],
     ["Tarro Blood", 'PvP'],
     ["Vulkar Highway", 'PvP'],
     ["The Shadowlands", 'PvP'],
-    ["Prophecy of the Five", 'PvP']]
+    ["Prophecy of the Five", 'PvP']
+  ]
 end
