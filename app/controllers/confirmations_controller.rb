c1749287846ce1095794e800d04dcec62dc9c0c7
Mike O'Toole <mike.otoole@digitalaugment.com>
2011-12-03 17:39:13 -0800

    Updated the confirmations controller to redirect to user profile.

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,19 @@
+###
+# Author::    DigitalAugment Inc. (mailto:info@digitalaugment.com)
+# Copyright:: Copyright (c) 2011 DigitalAugment Inc.
+# License::   Proprietary Closed Source
+#
+# This controller overrides Devise's Confirmations Controller
+###
+class ConfirmationsController < Devise::ConfirmationsController 
+  protected
+
+  # The path used after confirmation.
+  def after_confirmation_path_for(resource_name, resource)
+    if resource.is_a?(User)
+      user_profile_path(resource.user_profile)
+    else
+      after_sign_in_path_for(resource)
+    end  
+  end
+end
