714c8c67b1e14389285b9a893da2a5fb4752b9b5
Joe Weakley <joe.weakley@digitalaugment.com>
2011-12-06 15:34:45 -0800

    Updated role.

--- app/models/role.rb
+++ app/models/role.rb
@@ -13,7 +13,7 @@ class Role < ActiveRecord::Base
   has_many :permissions
   has_and_belongs_to_many :community_profiles
   has_many :user_profiles, :through => :community_profiles
-  accepts_nested_attributes_for :permissions, :allow_destroy => true
+  accepts_nested_attributes_for :permissions, :reject_if => lambda { |p| p[:permission_level].blank? }, :allow_destroy => true
 
 ###
 # Validators
@@ -28,13 +28,13 @@ class Role < ActiveRecord::Base
 
   def permissions_for_resource(resource)
     permission_match = self.permissions.find_by_subject_class_and_id_of_subject(resource.class.to_s,resource.id)
-    return permission_match ? permission_match : Permission.new(role: self, subject_class: resource.class, id_of_subject: resource.id, _destroy: true)
+    return permission_match ? permission_match : Permission.new(role: self, subject_class: resource.class, id_of_subject: resource.id)
   end
   def nested_permissions_for_resource(resource)
     case resource.class.to_s
       when "DiscussionSpace"
         permission_match = self.permissions.find_by_subject_class_and_id_of_parent("Discussion",resource.id)
-        return permission_match ? permission_match : Permission.new(role: self, subject_class: "Discussion", parent_association_for_subject: "discussion_space", id_of_parent: resource.id, _destroy: true)
+        return permission_match ? permission_match : Permission.new(role: self, subject_class: "Discussion", parent_association_for_subject: "discussion_space", id_of_parent: resource.id)
       else
         return nil
     end
