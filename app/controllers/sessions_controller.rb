12a110babcfe857954989be5328e3995aa4f385a
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-01-01 11:42:47 -0700

    Fixed BVR-391

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < Devise::SessionsController
     sign_out(current_admin_user) if current_admin_user
     current_user.update_attribute(:force_logout, false) if current_user and current_user.force_logout
     resource = warden.authenticate!(:scope => resource_name, :recall => "#{controller_path}#new")
-    set_flash_message(:notice, :signed_in, :display_name => current_user.display_name) if is_navigational_format?
+    set_flash_message(:notice, :user_signed_in, :display_name => current_user.display_name) if is_navigational_format?
     add_new_flash_message flash[:notice], 'success'
     flash.delete :notice
     session[:hide_announcements] = true
