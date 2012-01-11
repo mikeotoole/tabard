36ed0dac33a4ff06c36ca62bd96532cbef1fd448
Douglas Waltman II <doug@dougwaltman.com>
2012-01-10 16:39:30 -0800

    updated community form field partial

--- app/uploaders/background_image_uploader.rb
+++ app/uploaders/background_image_uploader.rb
@@ -26,12 +26,7 @@ class BackgroundImageUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     #"/images/fallback/" + [version_name, "default.png"].compact.join('_')
-    case version_name
-    when :standard
-      "/assets/community/bg.jpg"
-    else
-      "/assets/community/bg.jpg"
-    end
+    "/assets/application/blank.gif"
   end
 
   # Process files as they are uploaded:
@@ -44,6 +39,12 @@ class BackgroundImageUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :standard do
     process :resize_to_fill => [1280, 600]
+  end
+  version :large do
+    process :resize_to_fill => [900, 420]
+  end
+  version :thumbnail do
+    process :resize_to_fill => [320, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
