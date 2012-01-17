93b0b944f8e9a7c8dfd38aa123b36a302b07d03a
Joe Weakley <joe.weakley@digitalaugment.com>
2012-01-16 16:17:57 -0800

    Updated seed.

--- db/seeds/themes.rb
+++ db/seeds/themes.rb
@@ -4,11 +4,11 @@ unless @dont_run
   # Create Themes
   ###
 
-  Theme.create(name: "Crumblin", css: "crumblin")
-  Theme.create(name: "Droid", css: "droid")
-  Theme.create(name: "Metropolis", css: "metropolis")
-  Theme.create(name: "Red Rum", css: "redrum")
-  Theme.create(name: "Snow Tower", css: "snowtower")
-  Theme.create(name: "Station", css:"station")
+  Theme.create(name: "Crumblin", css: "crumblin", author: "Douglas Waltman", author_url: "http://digitalaugment.com")
+  #Theme.create(name: "Droid", css: "droid")
+  Theme.create(name: "Metropolis", css: "metropolis", author: "Alex Ruiz", author_url: "http://tarrzan.deviantart.com/")
+  #Theme.create(name: "Red Rum", css: "redrum", author: "Douglas Waltman", author_url: "http://digitalaugment.com")
+  Theme.create(name: "Snow Tower", css: "snowtower", author: "Igor Vitkovskiy", author_url: "http://m3-f.deviantart.com/")
+  Theme.create(name: "Station", css:"station", author: "Alex Ruiz", author_url: "http://tarrzan.deviantart.com/")
 
 end
