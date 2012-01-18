a05d8321c3b3164b94ad9de350f4fdba22fc9874
Joe Weakley <joe.weakley@digitalaugment.com>
2012-01-17 16:34:40 -0800

    Updated thumbnail.

--- db/seeds/themes.rb
+++ db/seeds/themes.rb
@@ -4,10 +4,10 @@ unless @dont_run
   # Create Themes
   ###
 
-  Theme.create(name: "Crumblin", css: "crumblin", thumbnail: "temp")
-  Theme.create(name: "Droid", css: "droid", background_author: "Alex Ruiz", background_author_url: "http://tarrzan.deviantart.com/", thumbnail: "temp")
-  Theme.create(name: "Metropolis", css: "metropolis", background_author: "Alex Ruiz", background_author_url: "http://tarrzan.deviantart.com/", thumbnail: "temp")
-  Theme.create(name: "Station", css:"station", background_author: "Alex Ruiz", background_author_url: "http://tarrzan.deviantart.com/", thumbnail: "temp")
-  Theme.create(name: "Snow Tower", css: "snowtower", background_author: "Igor Vitkovskiy", background_author_url: "http://m3-f.deviantart.com/", thumbnail: "temp")
+  Theme.create(name: "Crumblin", css: "crumblin", thumbnail: "crumblin")
+  Theme.create(name: "Droid", css: "droid", background_author: "Alex Ruiz", background_author_url: "http://tarrzan.deviantart.com/", thumbnail: "droid")
+  Theme.create(name: "Metropolis", css: "metropolis", background_author: "Alex Ruiz", background_author_url: "http://tarrzan.deviantart.com/", thumbnail: "metropolis")
+  Theme.create(name: "Station", css:"station", background_author: "Alex Ruiz", background_author_url: "http://tarrzan.deviantart.com/", thumbnail: "station")
+  Theme.create(name: "Snow Tower", css: "snowtower", background_author: "Igor Vitkovskiy", background_author_url: "http://m3-f.deviantart.com/", thumbnail: "snowtower")
 
 end
