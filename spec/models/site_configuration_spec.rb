d3b18fe02c4b0ea41745f2faae131b5b85829ebb
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-01-24 19:19:26 -0800

    Updated tests for site configuration

--- spec/models/site_configuration_spec.rb
+++ spec/models/site_configuration_spec.rb
@@ -11,5 +11,24 @@
 require 'spec_helper'
 
 describe SiteConfiguration do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "current_configuration" do
+    it "should create or find the only configuration" do
+      Rails.cache.delete('SiteConfiguration.first')
+      SiteConfiguration.all.size.should eq 0
+      @config = SiteConfiguration.current_configuration
+      SiteConfiguration.all.size.should eq 1
+      @config2 = SiteConfiguration.current_configuration
+      SiteConfiguration.all.size.should eq 1
+      @config.should eq @config2
+    end
+  end
+
+  describe "destroy" do
+    it "should delete SiteConfiguration" do
+      configuration = SiteConfiguration.create
+      SiteConfiguration.all.size.should eq 1
+      configuration.destroy
+      SiteConfiguration.exists?(configuration).should be_false
+    end
+  end
 end
