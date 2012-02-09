40164ce11a0e59104065da9e790a9027a1cf2305
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-02-09 09:44:39 -0800

    Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@ unless Rails.env.test?
   end
 
   if Rails.env.development?
-#     %w{ users characters communities roles_permissions discussions pages messages custom_forms }.each do |part|
-#       require File.expand_path(File.dirname(__FILE__))+"/seeds/#{part}.rb"
-#     end
+    %w{ users characters communities roles_permissions discussions pages messages custom_forms }.each do |part|
+      require File.expand_path(File.dirname(__FILE__))+"/seeds/#{part}.rb"
+    end
   elsif Rails.env.production?
     puts "Seeding 'bryan.rogers@digitalaugment.com' super admin"
     AdminUser.create!(:email => 'bryan.rogers@digitalaugment.com', 
