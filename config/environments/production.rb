a895ea2bd881140111105616254392ea0314fcff
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-03-03 14:36:27 -0800

    Fix bug with all JS not getting compiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ DaBvRails::Application.configure do
   config.action_controller.asset_host = "https://#{ENV['BV_ASSETS_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w(active_admin.css active_admin.js application/*.* fonts/*.* themes/*.* top_level/*.* javascripts/*.*)
+  config.assets.precompile += %w(active_admin.css active_admin.js application/*.* fonts/*.* themes/*.* top_level/*.* *.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
