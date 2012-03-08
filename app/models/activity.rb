bad8a3a20865314c27bc22358b6a0d91f6124054
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-03-08 08:53:53 -0800

    Fixed times for time zone support

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -61,14 +61,18 @@ class Activity < ActiveRecord::Base
     max_items = 50 unless max_items < 50
 
     if updated and updated[:since] and updated[:before]
-      @activities = Activity.ordered.where(activity).where('updated_at < :since AND updated_at > :before', updated).first(max_items.to_i)
-      @comments = Comment.not_deleted.ordered.where(activity).where('updated_at < :since AND updated_at > :before', updated).first(max_items.to_i)
+      since = updated[:since].in_time_zone('UTC')
+      before = updated[:before].in_time_zone('UTC')
+      @activities = Activity.ordered.where(activity).where('updated_at < ? AND updated_at > ?', since, before).first(max_items.to_i)
+      @comments = Comment.not_deleted.ordered.where(activity).where('updated_at < ? AND updated_at > ?', since, before).first(max_items.to_i)
     elsif updated and updated[:since]
-      @activities = Activity.ordered.where(activity).where('updated_at > :since', updated).first(max_items.to_i)
-      @comments = Comment.not_deleted.ordered.where(activity).where('updated_at > :since', updated).first(max_items.to_i)
+      since = updated[:since].in_time_zone('UTC')
+      @activities = Activity.ordered.where(activity).where('updated_at > ?', since).first(max_items.to_i)
+      @comments = Comment.not_deleted.ordered.where(activity).where('updated_at > ?', since).first(max_items.to_i)
     elsif updated and updated[:before]
-      @activities = Activity.ordered.where(activity).where('updated_at < :before', updated).first(max_items.to_i)
-      @comments = Comment.not_deleted.ordered.where(activity).where('updated_at < :before', updated).first(max_items.to_i)
+      before = updated[:before].in_time_zone('UTC')
+      @activities = Activity.ordered.where(activity).where('updated_at < ?', before).first(max_items.to_i)
+      @comments = Comment.not_deleted.ordered.where(activity).where('updated_at < ?', before).first(max_items.to_i)
     else
       @activities = Activity.ordered.where(activity).first(max_items.to_i)
       @comments = Comment.not_deleted.ordered.where(activity).first(max_items.to_i)
