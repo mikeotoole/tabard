9b53770fc937a26f4ee57f34fa01796ed1da414c
Douglas Waltman II <doug@dougwaltman.com>
2012-03-17 13:27:52 -0700

    updated themes and artwork BVR-550

--- db/seeds/themes.rb
+++ db/seeds/themes.rb
@@ -5,8 +5,8 @@ unless @dont_run
   ###
 
   Theme.create!(name: "Crumblin", css: "crumblin", thumbnail: "crumblin.jpg")
-  Theme.create!(name: "Cyborg", css: "cyborg", thumbnail: "cyborg.jpg")
-  Theme.create!(name: "Metropolis", css: "metropolis", thumbnail: "metropolis.jpg")
-  Theme.create!(name: "Wasteland", css:"wasteland", background_author: "Craig Soulsby", background_author_url: "http://xblitzcraigx.deviantart.com/", thumbnail: "wasteland.jpg")
-  Theme.create!(name: "Snow Tower", css: "snowtower", background_author: "Igor Vitkovskiy", background_author_url: "http://m3-f.deviantart.com/", thumbnail: "snowtower.jpg")
+  Theme.create!(name: "Cyborg", css: "cyborg", background_author: "Mac Rebisz", background_author_url: "http://maciejrebisz.com", thumbnail: "cyborg.jpg")
+  Theme.create!(name: "Metropolis", css: "metropolis", background_author: "Igor Vitkovskiy", background_author_url: "http://m3-f.deviantart.com", thumbnail: "metropolis.jpg")
+  Theme.create!(name: "Wasteland", css: "wasteland", background_author: "Craig Soulsby", background_author_url: "http://xblitzcraigx.deviantart.com", thumbnail: "wasteland.jpg")
+  Theme.create!(name: "Hailstone", css: "snowtower", background_author: "Mac Rebisz", background_author_url: "http://maciejrebisz.com", thumbnail: "hailstone.jpg")
 end
