ade710efc83a3c8c9eec0c1dca3ea1f8d9d69571
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-03-17 14:01:55 -0700

    Updated event for name.

--- spec/factories/event_factories.rb
+++ spec/factories/event_factories.rb
@@ -19,17 +19,4 @@ FactoryGirl.define do
   factory :private_event, :parent => :event do
     is_private { true }
   end
-  
-#   factory :invite do
-#     event_id { DefaultObjects.event.id }
-#     user_profile_id { DefaultObjects.user_profile.id }
-#   end
-#   
-#   factory :invite_to_wow_char, :parent => :invite do
-#     character_proxy_id { Factory.create(:character_proxy_with_wow_character).id }
-#   end
-#   
-#   factory :invite_to_swtor_char, :parent => :invite do
-#     character_proxy_id { Factory.create(:character_proxy_with_swtor_character).id }
-#   end
 end
