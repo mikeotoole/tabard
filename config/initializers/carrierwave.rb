7430f3917ded20c275e395b1e6acd22028b49bf5
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-07-27 12:21:00 -0700

    Updated Carrierwave config to use ENV var for bucket names.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,21 +1,10 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['BV_S3_KEY'],
-      :aws_secret_access_key  => ENV['BV_S3_SECRET']
-    }
-    config.fog_directory = 'guild.io'
-  end
-else
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['BV_DEV_S3_KEY'],
-      :aws_secret_access_key  => ENV['BV_DEV_S3_SECRET']
-    }
-    config.fog_directory = 'brutalvenom-development'
-  end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['BV_S3_KEY'],
+    :aws_secret_access_key  => ENV['BV_S3_SECRET']
+  }
+  config.fog_directory = ENV['BV_S3_BUCKET']
 end
 if Rails.env.test? or Rails.env.cucumber?
   CarrierWave.configure do |config|
