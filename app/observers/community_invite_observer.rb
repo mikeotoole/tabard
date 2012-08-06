1cc3ae0f8158b9d24eab6be39a46ebffb4b50c1a
Douglas Waltman II <doug@dougwaltman.com>
2012-08-06 16:24:17 -0700

    view work and message update for recruiting BVR-660

--- app/observers/community_invite_observer.rb
+++ app/observers/community_invite_observer.rb
@@ -14,7 +14,7 @@ class CommunityInviteObserver < ActiveRecord::Observer
     unless Rails.env.test?
       default_url_options[:host] = ENV["RAILS_ENV"] == 'production' ? "guild.io" : "lvh.me:3000"
       Message.create_system(subject: "You have been invited to #{community_invite.community_name}",
-                    body: "#{community_invite.sponsor_display_name} has invited you to [#{community_invite.community_name}](#{root_url(subdomain: community_invite.community_subdomain)}). You can apply [here](#{new_community_application_url(subdomain: community_invite.community_subdomain)})",
+                    body: "#{community_invite.sponsor_display_name} has invited you to [join the \"#{community_invite.community_name}\" community](#{new_community_application_url(subdomain: community_invite.community_subdomain)}).",
                     to: [community_invite.applicant_id])
     end
   end
