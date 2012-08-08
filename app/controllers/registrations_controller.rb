c3653c3254c72180b209d9e195d0d2a5a1c84870
Joe Weakley <joe.weakley@digitalaugment.com>
2012-08-08 13:59:00 -0700

    Test update.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -107,6 +107,7 @@ class RegistrationsController < Devise::RegistrationsController
       sign_in(resource_name, resource)
       redirect_to after_sign_in_path_for(resource)
     else
+      add_new_flash_message "#{resource.errors.to_yaml}", "error"
       render :reinstate_account
     end
   end
