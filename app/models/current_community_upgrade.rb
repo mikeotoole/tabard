58bf2a79f7ed3f437655a91cdc04290e5e4e77ee
Joe Weakley <joe.weakley@digitalaugment.com>
2012-08-30 15:52:48 -0700

    Updated saving logic.

--- app/models/current_community_upgrade.rb
+++ app/models/current_community_upgrade.rb
@@ -3,6 +3,29 @@ class CurrentCommunityUpgrade < ActiveRecord::Base
   belongs_to :community, inverse_of: :current_community_upgrades
   belongs_to :community_upgrade, inverse_of: :current_community_upgrades
 
+  before_save :update_amounts
+
+  def update_amounts
+    if number_in_use_changed?
+      if number_in_use_was > number_in_use
+        subcription_amount = number_in_use_was
+      else
+        subcription_amount = number_in_use
+      end
+    else
+      return true
+    end
+  end
+
+  def current_amount
+    return number_in_use if current_plan_expiration_date.blank?
+    if current_plan_expiration_date > Time.now
+      return number_in_use
+    else
+      return subcription_amount
+    end
+  end
+
   def total_price_per_month_in_cents
     unless self.marked_for_destruction?
       self.number_in_use * self.community_upgrade.price_per_month_in_cents
@@ -10,6 +33,7 @@ class CurrentCommunityUpgrade < ActiveRecord::Base
       0
     end
   end
+
   def total_price_per_month_in_dollars
     self.total_price_per_month_in_cents/100.0
   end
