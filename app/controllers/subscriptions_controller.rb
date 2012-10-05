17c33fc9c867c819cbe0979fa53502c1d63a0065
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-10-05 14:51:50 -0700

    Fixed subscription controller to use @invoice.

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -26,29 +26,19 @@ class SubscriptionsController < ApplicationController
     if @community.blank?
       raise CanCan::AccessDenied
     else
-      @current_invoice = current_user.current_invoice
       # Get invoice item for current plan.
-      @current_plan_invoice_item = @current_invoice.recurring_plan_invoice_item_for_community(@community)
-
-      current_plan = @current_plan_invoice_item.item
-
-      existing_upgrades_invoice_items = @current_invoice.recurring_upgrade_invoice_items_for_community(@community)
-
-      existing_upgrades = existing_upgrades_invoice_items.map{|i| i.item}
-      new_upgrades = current_plan.community_upgrades.delete_if{|plan| existing_upgrades.include?(plan) }
-      new_upgrades_invoice_items = new_upgrades.map{ |item| @current_invoice.invoice_items.new({item: item, quantity: 0, community_id: @community.id}, without_protection: true)}
-
-      @all_upgrades_invoice_items = existing_upgrades_invoice_items + new_upgrades_invoice_items
+      load_invoice_items
     end
   end
 
   # PUT /subscriptions/:community_id
   def update
-    @invoice = current_user.current_invoice
     @stripe_card_token = params[:stripe_card_token]
     begin
       if @invoice.update_attributes_with_payment(params[:invoice], @stripe_card_token)
         flash[:success] = "Your plan has been changed"
+      else
+        load_invoice_items
       end
     rescue Stripe::StripeError => e
       logger.error "StripeError: #{e.message}"
@@ -71,5 +61,20 @@ protected
   def load_variables
     @community = current_user.owned_communities.find_by_id(params[:community_id])
     @available_plans = CommunityPlan.available
+    @invoice = current_user.current_invoice
+  end
+
+  def load_invoice_items
+    @current_plan_invoice_item = @invoice.recurring_plan_invoice_item_for_community(@community)
+
+    current_plan = @current_plan_invoice_item.item
+
+    existing_upgrades_invoice_items = @invoice.recurring_upgrade_invoice_items_for_community(@community)
+
+    existing_upgrades = existing_upgrades_invoice_items.map{|i| i.item}
+    new_upgrades = current_plan.community_upgrades.delete_if{|plan| existing_upgrades.include?(plan) }
+    new_upgrades_invoice_items = new_upgrades.map{ |item| @invoice.invoice_items.new({item: item, quantity: 0, community_id: @community.id}, without_protection: true)}
+
+    @all_upgrades_invoice_items = existing_upgrades_invoice_items + new_upgrades_invoice_items
   end
 end
