af4c219e2d80c423e462d64d9f051a24d26f0f94
Joe Weakley <joe.weakley@digitalaugment.com>
2012-10-09 16:15:28 -0700

    Updated logging for webhooks.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,15 +2,17 @@ Stripe.api_key = ENV['STRIPE_PRIVATE_KEY']
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
 
 StripeEvent.setup do
-  subscribe 'charge.failed' do |event|
-    # Define subscriber behavior
+  subscribe 'charge.failed', 'charge.succeeded', 'charge.refunded' do |event|
+    logger.info "STRIPE_EVENT:CHARGE: #{event.to_yaml}"
+    # Charge events
   end
 
-  subscribe 'customer.created', 'customer.updated' do |event|
-    # Handle multiple event types
+  subscribe 'transfer.created', 'transfer.updated', 'transfer.failed'do |event|
+    logger.info "STRIPE_EVENT:TRANSFER: #{event.to_yaml}"
+    # Transfer events
   end
 
   subscribe do |event|
-    # Handle all event types - logging, etc.
+    logger.info "STRIPE_EVENT: #{event.to_yaml}"
   end
 end
