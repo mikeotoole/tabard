23bf2f088411c667864c242e1cbec58a57675bdc
Douglas Waltman II <doug@dougwaltman.com>
2012-10-11 11:13:07 -0700

    fixed flash responder

--- lib/flash_responder.rb
+++ lib/flash_responder.rb
@@ -12,14 +12,13 @@ module FlashResponder
     unless get? || options.delete(:flash) == false
       namespace = controller.controller_path.split('/')
       namespace << controller.action_name
-      controller.flash[:messages] = Array.new unless controller.flash[:messages]
       if has_errors?
         if options.delete(:error_behavior) == :list or resource.errors.size == 1
           resource.errors.full_messages.map{ |error|
-            controller.flash[:messages] << { class: 'alert', title: '', body: error }
+            controller.flash[:alert] = error
           }
         else
-          controller.flash[:messages] << { class: 'alert', title: 'Epic Fail', body: "Multiple errors were found." }
+          controller.flash[:alert] = "Multiple errors were found."
         end
       end
     end
