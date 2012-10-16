15531379aba2d4c450781f13743f7efdb4ffa40d
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-10-16 10:11:27 -0700

    Fixed issue with logger in Stripe config.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,16 +3,16 @@ STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
 
 StripeEvent.setup do
   subscribe 'charge.failed', 'charge.succeeded', 'charge.refunded' do |event|
-    logger.info "STRIPE_EVENT:CHARGE: #{event.to_yaml}"
+    puts "STRIPE_EVENT:CHARGE: #{event.to_yaml}"
     # Charge events
   end
 
   subscribe 'transfer.created', 'transfer.updated', 'transfer.failed'do |event|
-    logger.info "STRIPE_EVENT:TRANSFER: #{event.to_yaml}"
+    puts "STRIPE_EVENT:TRANSFER: #{event.to_yaml}"
     # Transfer events
   end
 
   subscribe do |event|
-    logger.info "STRIPE_EVENT: #{event.to_yaml}"
+    puts "STRIPE_EVENT: #{event.to_yaml}"
   end
 end
