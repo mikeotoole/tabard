fe2df67e57aa721e0cc4bddb9274bc4ac6a1de4a
Mike O'Toole <mike.otoole@digitalaugment.com>
2012-10-19 18:02:05 -0700

    Fixed activity creation.

--- app/observers/character_observer.rb
+++ app/observers/character_observer.rb
@@ -11,16 +11,19 @@ class CharacterObserver < ActiveRecord::Observer
   # Creates an activity when a character is updated.
   def after_update(character)
     if character.changed?
-      change = character.changed == ["avatar", "updated_at"] ? "avatar" : "edited"
-
-      Activity.create!({user_profile: character.user_profile,
-                        target: character.character_proxy,
-                        action: change}, without_protection: true)
+      unless (character.changed == ["avatar", "updated_at"] and not character.avatar.cached?.present?)
+        change = (character.changed == ["avatar", "updated_at"] and character.avatar.cached?.present?) ? "avatar" : "edited"
+        Activity.create!({user_profile: character.user_profile,
+                          target: character.character_proxy,
+                          action: change}, without_protection: true)
+      end
     end
+    return true
   end
 
   # removes comment
   def after_destroy(character)
     Activity.where(target_type: character.character_proxy.class.to_s, target_id: character.character_proxy.id).destroy_all
+    return true
   end
 end
--- app/observers/user_profile_observer.rb
+++ app/observers/user_profile_observer.rb
@@ -12,27 +12,31 @@ class UserProfileObserver < ActiveRecord::Observer
     Activity.create!( {user_profile: user_profile,
                       target: user_profile,
                       action: "joined"}, without_protection: true)
+    return true
   end
 
   # Creates a new activity when a user updates profile display_name, description, or title.
   def after_update(user_profile)
     if user_profile.changed?
+      unless (user_profile.changed == ["avatar", "updated_at"] and not user_profile.avatar.cached?.present?)
+        change = user_profile.display_name_changed? ? "player name" : nil
+        change = change ? "profile" : "description" if user_profile.description_changed?
+        change = change ? "profile" : "title" if user_profile.title_changed?
+        change = change ? "profile" : "avatar" if user_profile.avatar.cached?.present?
+        change = change ? change : "profile"
 
-      change = user_profile.display_name_changed? ? "player name" : nil
-      change = change ? "profile" : "description" if user_profile.description_changed?
-      change = change ? "profile" : "title" if user_profile.title_changed?
-      change = change ? "profile" : "avatar" if user_profile.avatar_changed?
-      change = change ? change : "profile"
-
-      Activity.create!( {user_profile: user_profile,
-                        target: user_profile,
-                        action: change}, without_protection: true)
+        Activity.create!( {user_profile: user_profile,
+                          target: user_profile,
+                          action: change}, without_protection: true)
+      end
     end
+    return true
   end
 
   # removes activity
   def after_destroy(user_profile)
     target = user_profile
     Activity.where(target_type: target.class.to_s, target_id: target.id).destroy_all
+    return true
   end
 end
