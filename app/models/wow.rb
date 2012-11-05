8fa6209b3a4e06889b77321e87d82c78b7b331bb
Joe Weakley <joe.weakley@digitalaugment.com>
2012-11-05 15:34:47 -0800

    Improved wow.

--- app/models/wow.rb
+++ app/models/wow.rb
@@ -85,6 +85,21 @@ class Wow < Game
   def new_character(params)
     WowCharacter.new(params)
   end
+
+  def set_specific_community_game_attributes
+    return true
+  end
+
+  def update_community_game_attributes(community_game)
+    server = nil
+    server_array.each do |server_info|
+      if server_info[:name] == community_game.server_name
+        server = server_info
+        break
+      end
+    end
+    community_game.server_type = server[:type] unless server.blank?
+  end
 end
 
 # == Schema Information
