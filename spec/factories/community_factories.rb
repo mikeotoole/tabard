196e591c3404a564c72d96a5898b2144a31b83da
Joe Weakley <joe.weakley@digitalaugment.com>
2012-12-19 10:12:50 -0800

    Fixed factory.

--- spec/factories/community_factories.rb
+++ spec/factories/community_factories.rb
@@ -8,17 +8,25 @@ FactoryGirl.define do
     factory :pro_community do
       after(:create) do |community|
         community_plan = FactoryGirl.create(:pro_community_plan)
+        community_plan = CommunityPlan.find_by_title("Pro Community")
         token = Stripe::Token.create(
             :card => {
             :number => "4242424242424242",
             :exp_month => 8,
             :exp_year => 2023,
-            :cvc => 314
+            :cvc => 314,
+            :address_line1 => '710 George Washington Way',
+            :address_city => 'Richland',
+            :address_state => 'WA',
+            :address_zip => '99352'
           },
         )
+        invoice_hash = { "invoice_items_attributes" => { "0" => { "community_id"=>"#{community.id}",
+                                                                  "item_type"=>"CommunityPlan",
+                                                                  "quantity"=>"1",
+                                                                  "item_id"=>"#{community_plan.id}" }}}
         invoice = community.admin_profile.user.current_invoice
-        invoice.invoice_items.new({community: community, item: community_plan, quantity: 1}, without_protection: true)
-        invoice.update_attributes_with_payment(nil, token.id)
+        invoice.update_attributes_with_payment(invoice_hash, token.id)
       end
 
       factory :pro_community_with_user_upgrade do
