2e9c7df752ce5c1b44fc40e1ea5ce8b9cad4e6eb
Joe Weakley <joe.weakley@digitalaugment.com>
2013-01-23 12:10:20 -0800

    Updated sampel test.

--- spec/requests/communities_spec.rb
+++ spec/requests/communities_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 describe "Communities" do
-  #TODO
+  let(:billy) { create(:billy) }
+  describe "new" do
+    it "should be accessable if signed in" do
+      login_as(billy, :scope => :user)
+      visit new_community_url(subdomain: "secure")
+      page.should have_content("Create a Community")
+      Warden.test_reset!
+    end
+    it "should not be accessable if signed in" do
+      visit new_community_url(subdomain: "secure")
+      page.should_not have_content("Create a Community")
+    end
+  end
+
 end
