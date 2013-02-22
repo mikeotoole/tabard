2a146509e86f3653cfd0d06b39be2f7cd61b5296
Joe Weakley <joe.weakley@digitalaugment.com>
2013-02-22 11:27:47 -0800

    Updated with new avatar finder. BVR-948.

--- app/models/characters/minecraft_character.rb
+++ app/models/characters/minecraft_character.rb
@@ -6,6 +6,28 @@
 # This class represents a Minecraft character.
 ###
 class MinecraftCharacter < Character
+
+  def find_and_upload_avatar_from_game
+    begin
+      begin
+        head_image = MiniMagick::Image.open("http://s3.amazonaws.com/MinecraftSkins/#{self.name}.png")
+        hat_image = MiniMagick::Image.open("http://s3.amazonaws.com/MinecraftSkins/#{self.name}.png")
+      rescue
+        head_image = MiniMagick::Image.open("http://minecraft.net/skin/char.png")
+        hat_image = MiniMagick::Image.open("http://minecraft.net/skin/char.png")
+      end
+      # (8,8,8,8) (48,8,8,8)
+      head_image.crop("8x8+8+8")
+      hat_image.crop("8x8+40+8")
+      final_image = head_image.composite(hat_image) do |c|
+        c.gravity "center"
+      end
+      final_image.sample("240x240")
+      self.avatar = final_image
+      self.save!
+    rescue
+    end
+  end
 end
 
 # == Schema Information
