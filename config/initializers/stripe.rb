71d5fcf2f7cc21b663dd4657e58b1a0de27d3653
Mike O'Toole <mike.otoole@digitalaugment.com>
2013-03-01 13:15:02 -0700

    Updated stripe events output. Updated stripe and stripe_event gems.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,17 +1,24 @@
 Stripe.api_key = ENV['STRIPE_PRIVATE_KEY']
 STRIPE_PUBLIC_KEY = ENV['STRIPE_PUBLIC_KEY']
+
 StripeEvent.setup do
-  subscribe 'charge.failed', 'charge.succeeded', 'charge.refunded', 'charge.disputed' do |event|
-    puts "STRIPE_EVENT:CHARGE: #{event.to_yaml}"
-    # Charge events
+  subscribe 'charge.failed', 'charge.refunded', 'charge.disputed' do |event|
+    puts "ALERT_ERROR STRIPE_EVENT:CHARGE: #{event.to_yaml}"
+  end
+
+  subscribe 'charge.succeeded' do |event|
+    #puts "STRIPE_EVENT:CHARGE: #{event.to_yaml}"
+  end
+
+  subscribe 'customer.deleted' do |event|
+    puts "ALERT_ERROR STRIPE_EVENT:CUSTOMER:DELETED: #{event.to_yaml}"
   end
 
   subscribe 'transfer.created', 'transfer.updated', 'transfer.failed'do |event|
-    puts "STRIPE_EVENT:TRANSFER: #{event.to_yaml}"
-    # Transfer events
+    puts "ALERT_ERROR STRIPE_EVENT:TRANSFER: #{event.to_yaml}"
   end
 
-  subscribe do |event|
-    puts "STRIPE_EVENT: #{event.to_yaml}"
-  end
+#   subscribe do |event|
+#     puts "STRIPE_EVENT: #{event.to_yaml}"
+#   end
 end
