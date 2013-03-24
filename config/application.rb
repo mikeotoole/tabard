c11c501e6159ee77c3a49883163158c8c60e482e
Mike O'Toole <mike.otoole@digitalaugment.com>
2013-03-24 07:24:34 -0600

    Added validation_code to filted params.

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module DaBvRails
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :password_confirmation]
+    config.filter_parameters += [:password, :password_confirmation, :validation_code]
 
     # Filter Stripe Webhook Params
     config.filter_parameters += [:active_card]
