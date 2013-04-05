e51e628ffa7ab57b0e1313bb76b1076421c340a5
Douglas Waltman II <doug@dougwaltman.com>
2013-04-05 09:54:43 -0700

    Fixed broken action_items menu dismiss link and some css.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215184154) do
+ActiveRecord::Schema.define(:version => 20130211233442) do
 
   create_table "acknowledgements", :force => true do |t|
     t.integer  "community_profile_id"
